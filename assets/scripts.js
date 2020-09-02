4e3559d78efbe4cc4b20a6b947524c296b857418
buppagi <buppagistar@gmail.com>
2020-09-03 00:00:45 +0900

    태그 적용 완료, 모드 변경
    태그 적용완료, 다크모드, 화이트 모드 적용

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,37 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-
-  $("[data-tag]").click((e) => {
-    currentTag = e.target.dataset.tag;
-    filterByTagName(currentTag);
-  })
-
-  function filterByTagName(tagName) {
-    $('.hidden').removeClass('hidden');
-    $('.post-wrapper').each((index, elem) => {
-      if (!elem.hasAttribute(`data-${tagName}`)) {
-        $(elem).addClass('hidden');
-      }
-    });
-    $(`.tag`).removeClass('selected');
-    $(`.tag[data-tag=${tagName}]`).addClass('selected');
-  }
-
-  let currentTag = "";
-  const queryTag = getQuery().tag;
-  if (queryTag) {
-    currentTag = queryTag;
-    filterByTagName(currentTag)
-  }
 });
-
-function getQuery() {
-  var params = {};
-  window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (str, key, value) { params[key] = value; });
-  return params;
-}
-
-function updateQueryString(tagName) {
-  const path = `${location.protocol}//${location.host}${location.pathname}?tag=${tagName}`;
-  window.history.replaceState({ path }, '', path);
-}
